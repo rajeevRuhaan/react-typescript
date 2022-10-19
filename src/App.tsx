1f8b221cbc3fc1f446cc1e0c530fda27e9c186ee
Rajeev <s2100155@edu.bc.fi>
2022-10-19 16:12:21 +0300

    script rewriting

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,27 @@ import Greetings from './components/Greetings';
 function App() {
   return (
     <React.Fragment>
-          <div className="container mt-3">
-        <div className="grid">
-          <div className="row">
-            <div className="col">
-              <p className="h3 fw-bold text-success">App Component</p>
-              <p className="fst-italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias dolorem et, iure doloribus adipisci ipsum sequi recusandae ex nisi necessitatibus corrupti odit neque accusantium eos deserunt beatae totam! Autem, tenetur!</p>
-            <button className="btn btn-success btn-sm">
-              <i className='fa fa-book mx-1'></i>Read More</button>
+        <div className="container mt-3">
+          <div className="grid">
+            <div className="row">
+              <div className="col">
+                <p className="h3 fw-bold text-success">App Component</p>
+                <p className="fst-italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias dolorem et, iure doloribus adipisci ipsum sequi recusandae ex nisi necessitatibus corrupti odit neque accusantium eos deserunt beatae totam! Autem, tenetur!</p>
+              <button className="btn btn-success btn-sm">
+                <i className='fa fa-book mx-1'></i>Read More</button>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <Counter />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <Greetings />
+              </div>
             </div>
           </div>
-          <div className="row">
-            <div className="col">
-              <Counter />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <Greetings />
-            </div>
-          </div>
-        </div>
       </div>
       
     </React.Fragment>
