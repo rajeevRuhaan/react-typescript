5820b4d4422f1143cff3f76858be813e69596c8d
Rajeev <s2100155@edu.bc.fi>
2022-10-19 11:21:29 +0300

    props for functional and class component

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,16 @@ import './App.css';
 function App() {
   return (
     <React.Fragment>
-      <h2 className='text-warning'>Raw React project with type script</h2>
-      <p>Set up bootstrap, fontawesome and ubuntu font</p>
+      <div className="container mt-3">
+        <div className="grid">
+          <div className="row">
+            <div className="col">
+              <p className="h3 fw-bold text-success">App Component</p>
+              <p className="fst-italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias dolorem et, iure doloribus adipisci ipsum sequi recusandae ex nisi necessitatibus corrupti odit neque accusantium eos deserunt beatae totam! Autem, tenetur!</p>
+            </div>
+          </div>
+        </div>
+      </div>
       
     </React.Fragment>
   );
